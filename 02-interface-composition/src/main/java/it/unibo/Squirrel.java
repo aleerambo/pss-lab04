7602f7d7a70fef245ae565dd9d659db1429562e3
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-10-29 12:00:27 +0100

    UPDATE kangaroo caneat method

--- 02-interface-composition/src/main/java/it/unibo/Squirrel.java
+++ 02-interface-composition/src/main/java/it/unibo/Squirrel.java
@@ -1,5 +1,23 @@
 package it.unibo;
 
 public class Squirrel implements Animal{
-    
+    private double averageWeight;
+    private boolean canEatVegetables;
+    private boolean canEat;
+
+    public double averageWeight() {
+        return this.averageWeight = 55;
+    }
+
+    public boolean canEatVegetables() {
+        return this.canEatVegetables = true;
+    }
+
+    public boolean canEat(Animal a) {
+        if(this.averageWeight > a.averageWeight()){
+            return this.canEat = true;
+        }else {
+            return this.canEat = false;
+        }
+    }
 }
