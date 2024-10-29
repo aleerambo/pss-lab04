9ec9edc25cc91cfb045ddc398ad269497beee5bf
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-10-29 16:46:08 +0100

    UPDATED korat class and its methods

--- 02-interface-composition/src/main/java/it/unibo/KoratCat.java
+++ 02-interface-composition/src/main/java/it/unibo/KoratCat.java
@@ -1,5 +1,23 @@
 package it.unibo;
 
 public class KoratCat implements Animal{
-    
+    private final double averageWeight;
+    private final boolean canEatVegetables;
+
+    public KoratCat(){
+        this.averageWeight = 4;
+        this.canEatVegetables = false;
+    }
+
+    public double averageWeight() {
+        return this.averageWeight;
+    }
+
+    public boolean canEatVegetables() {
+        return this.canEatVegetables;
+    }
+
+    public boolean canEat(Animal a) {
+        return a.averageWeight() < this.averageWeight;
+    }
 }
