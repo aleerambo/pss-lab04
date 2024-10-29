0246b1ff9cd7a2e05ad8fbdcffcdf42c11654165
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-10-29 11:54:59 +0100

    UPDATED Kangaroo class

--- 02-interface-composition/src/main/java/it/unibo/Kangaroo.java
+++ 02-interface-composition/src/main/java/it/unibo/Kangaroo.java
@@ -1,5 +1,19 @@
 package it.unibo;
 
 public class Kangaroo implements Animal {
-    
+    private double averageWeight;
+    private boolean canEatVegetables;
+    private boolean canEat;
+
+    public double averageWeight() {
+        return this.averageWeight = 55;
+    }
+
+    public boolean canEatVegetables() {
+        return this.canEatVegetables = true;
+    }
+
+    public boolean canEat(Animal a) {
+        return this.canEat = false;
+    }
 }
