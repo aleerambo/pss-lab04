df125f569a70cf331be27617c7bc4a1e6530edd4
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-10-29 16:45:40 +0100

    UPDATED siamese class and its methods

--- 02-interface-composition/src/main/java/it/unibo/SiameseCat.java
+++ 02-interface-composition/src/main/java/it/unibo/SiameseCat.java
@@ -1,5 +1,23 @@
 package it.unibo;
 
 public class SiameseCat implements Animal {
-    
+    private final double averageWeight;
+    private final boolean canEatVegetables;
+
+    public SiameseCat(){
+        this.averageWeight = 4;
+        this.canEatVegetables = false;
+    }
+
+    public double averageWeight() {
+        return this.averageWeight;
+    }
+
+    public boolean canEatVegetables() {
+        return this.canEatVegetables;
+    }
+
+    public boolean canEat(Animal a) {
+        return a.averageWeight() < this.averageWeight;
+    }
 }
